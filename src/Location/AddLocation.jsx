42407ba463ebf9a8f1c6b5f7cfd7e29a5ba014c4
chrismcglynn <chris@mcglynnenterprises.com>
2018-12-12 22:23:29 -0700

    setting up axios call to get weather data in componentDidMount()

--- src/Location/AddLocation.jsx
+++ src/Location/AddLocation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import AddLocationCard from './AddLocationCard';
 
 
@@ -7,6 +8,17 @@ class AddLocation extends Component {
   state = {
     city: null,
     state: null,
+  }
+
+  componentDidMount() {
+    const api_key = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+    axios
+      .get(
+        `api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=${api_key}`
+      )
+      .then(res => {
+        console.log(res);
+      });
   }
 
   handleChange = (e) => {
