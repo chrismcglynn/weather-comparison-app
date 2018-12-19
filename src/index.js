1dc1933bbd7e14a599c2a2ab16a22701582f7505
chrismcglynn <chris@mcglynnenterprises.com>
2018-12-19 16:13:32 -0700

    setting up reducer for Redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReduxer';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/reducers/rootReduxer.js
@@ -0,0 +1,9 @@
+const initState = {
+  locations: [],
+}
+
+const rootReducer = (state = initState, action) => {
+  return state;
+}
+
+export default rootReducer
